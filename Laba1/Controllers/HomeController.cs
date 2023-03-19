57ec7e3f295690eb7d5db362eafb48e301093340
MaksMaksimych <99993879+MaksMaksimych@users.noreply.github.com>
2023-03-19 23:02:13 +0300

    Add files via upload

---
+++ Laba1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laba1.Models;
+
+namespace Laba1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse x)
+        {
+            switch(x.Operation)
+            {
+                case "+":
+                    x.Result = (decimal)(x.Operand1 + x.Operand2);
+                    break;
+                case "-":
+                    x.Result = (decimal)(x.Operand1 - x.Operand2);
+                    break;
+                case "*":
+                    x.Result = (decimal)(x.Operand1 * x.Operand2);
+                    break;
+                case "/":
+                    x.Result = (decimal)(x.Operand1 / x.Operand2);
+                    break;
+            }
+
+            ViewBag.ResultSecond = 10;
+            
+            return View("Thanks", x);
+        }
+
+        
+
+    }
+}
---
+++ Laba1/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laba1.Models
+{
+    public class GuestResponse
+    {
+        public ushort Operand1 { get; set; }
+        public ushort Operand2 { get; set; }
+        public string Operation { get; set; }
+        public decimal Result { get; set; }
+    }
+
+}
